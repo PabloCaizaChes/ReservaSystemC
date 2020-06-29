3fe7d97a4757b8ad10a63f13443224f8a34e5bf0
Pablo Caiza <caizap@hotmail.com>
2020-06-29 14:13:50 -0500

    agregando un snackbar en la funcion presionar hacia atras

--- app/src/main/java/com/example/reservasystemc/MainActivity.kt
+++ app/src/main/java/com/example/reservasystemc/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.preference.PreferenceManager
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +13,10 @@ import java.util.prefs.Preferences
 import com.example.reservasystemc.PreferenceHelper.get
 import com.example.reservasystemc.PreferenceHelper.set
 class MainActivity : AppCompatActivity() {
+ //cuando se usa by lazy snackbar no tiene valor y cuando se quiera usar por primera vez hay recien se va a inicializar
+    private val snackBar by lazy {
+        Snackbar.make(mainLayout,R.string.press_back_again,Snackbar.LENGTH_SHORT)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,4 +63,11 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 
+    override fun onBackPressed() {
+        if(snackBar.isShown)
+        snackBar.show()
+        else
+        super.onBackPressed()
+
+    }
 }
